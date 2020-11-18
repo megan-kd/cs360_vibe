4cf19bbdfaa300f068173eb011265ad6bb10b7a2
Dragonnova5091 <esnakemaster1@gmail.com>
2020-11-17 16:05:53 -0800

    added first and last names

--- models/user.js
+++ models/user.js
@@ -4,6 +4,8 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
   {
+    firstName: {type: String, required: true},
+    lastname: {type: String, required: true},
     username: {type: String, required: true},
     password: {type: String, required: true},
     SpotifyUsername: {type: String},
