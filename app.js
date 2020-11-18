0f386a790248b5f1d1fb29afb76b225364546586
jepsonlogan <jeps6098@pacificu.edu>
2020-11-18 15:13:28 -0800

    Remade project, should work now

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-var registerRouter = require('./routes/register')
+var registerRouter = require('./routes/register');
 
 var app = express();
-
 
 var mongoose = require('mongoose');
 
@@ -22,10 +20,9 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
