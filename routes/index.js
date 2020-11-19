f27df6f1927bc44c0bb2a3367d074f7d1faa7b2b
Garret Katayama <garret.katayama@gmail.com>
2020-11-18 23:32:36 -0800

    view changes and added css

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let current = new Date();
+let month = current.getMonth();
+let day = current.getDate();
+let year = current.getFullYear();
+let date = month + "/" + day + "/" + year;
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Vibe of the Day', date: date });
 });
 
 module.exports = router;
