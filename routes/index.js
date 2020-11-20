fb1cf7df5f0d4665cedb3143cd3616ce4d25b151
Garret Katayama <garret.katayama@gmail.com>
2020-11-19 18:30:40 -0800

    added second column containing song search to home page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 let current = new Date();
+
 let month = current.getMonth();
 let day = current.getDate();
 let year = current.getFullYear();
+
 let date = month + "/" + day + "/" + year;
 
 router.get('/', function(req, res, next) {
